f87c585a15660ac98f4e24808fec92e77830aeca
skr@Precision7250 <krsheshu@gmail.com>
2022-06-08 15:59:17 +0200

    c++ tutorials: created a multiple source makefile for individual exes

---
+++ programming_principles_practice_c++/chapter_3/src/objects1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+std::cout << "Please enter your first name and second name\n";
+std::string first_name, secondname;
+// integer variable
+std::cin >> first_name >> secondname;
+std::cout << "Hello, " << first_name << ' ' << secondname << "\n";
+
+}
