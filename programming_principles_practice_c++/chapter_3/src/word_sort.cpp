051d18d8da660ca576190036f2d4c13ae26f4b58
Sheshu Kalaparmbathu Ramanandan <Sheshu.Ramanandan@smartray.com>
2022-06-28 13:53:27 +0200

    c++ tutorials:clang format introduced on save

---
+++ programming_principles_practice_c++/chapter_3/src/word_sort.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector<std::string> words;
+
+  for (std::string temp; std::cin >> temp;) {
+    // read whitespace-separated words
+    words.push_back(temp);  // put into vector
+    std::cout << temp << '\n';
+  }
+
+  std::cout << "Number of words: " << words.size() << '\n';
+  std::sort(words.begin(), words.end());
+
+  // sort the words
+  for (int i = 0; i < int(words.size()); ++i)
+    if (i == 0 || words[i - 1] != words[i])  // is this a new word?
+      std::cout << words[i] << "\n";
+}
