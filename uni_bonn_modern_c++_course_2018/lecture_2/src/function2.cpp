00be281c7a4e675573c32b41f58eee5f87c9df3e
Sheshu Ramanandan <krsheshu@gmail.com>
2022-09-30 14:13:24 +0200

    c++ tutorials: tested function overloading

---
+++ uni_bonn_modern_c++_course_2018/lecture_2/src/function2.cpp
@@ -0,0 +1,29 @@
+// Copyright: Free
+// Author: krsheshu
+// Brief:
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void Func ( const string& str ) {
+
+  cout << str << " is a string\n";
+
+}
+
+void Func ( const int integer ) {
+
+  cout << integer << " is an integer\n";
+
+}
+
+
+int main () {
+
+  Func ( "Ananthu" );
+  Func ( 10 );
+
+  return 0;
+}
